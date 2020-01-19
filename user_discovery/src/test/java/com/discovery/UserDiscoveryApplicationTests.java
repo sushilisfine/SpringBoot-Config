2a059b86d95353f855d9b9455da81d082ddc7f2a
Sushil Yadav (NCS) <sushil.yadav@in.ncs-i.com>
2020-01-19 16:36:59 +0530

    jg

---
+++ user_discovery/src/test/java/com/discovery/UserDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discovery;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class UserDiscoveryApplicationTests {
+
+
+	public void contextLoads() {
+	}
+
+}
